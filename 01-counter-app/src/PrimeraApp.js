5dd49bf7fabea2681a759142c3bbbea2ed3055ea
Fernando Martínez <frmartinezj@achs.cl>
2021-04-04 21:31:16 -0400

    Add 01-CounterApp

---
+++ 01-counter-app/src/PrimeraApp.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+    const devs = {
+        nombre: 'Fernando',
+        edad: 66
+    }
+
+   return <>
+        <h1>{ saludo }</h1>
+        <pre>{JSON.stringify(devs, null, 3)}</pre>
+        <p>{ subtitulo }</p>
+    </>;
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Esta es mi primera app'
+}
+
+export default PrimeraApp;
